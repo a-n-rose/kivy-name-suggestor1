02b1421633d47132d7a14e0ad254d4064e3f7f26
a-n-rose <37121771+a-n-rose@users.noreply.github.com>
2018-03-07 22:21:17 +0100

    Update nametxt2csv.py
    small edit - naming .csv file

--- nametxt2csv.py
+++ nametxt2csv.py
@@ -1,9 +1,8 @@
 '''
 works in directory of where .txt files from SSA's name database are saved
-puts most recent list to .csv with column names
+puts most recent list to namelist[year].csv with column names "name", "sex", "popularity"
 '''
 
-import numpy as np
 import pandas as pd
 import glob
 import os
@@ -15,8 +14,7 @@ for txt in glob.glob('*.txt'):
 text_files = sorted(text_files, reverse = True)
 latest_list = text_files[0]
 filename = os.path.splitext(latest_list)[0]
-filename = filename[10::]
+year = filename[-4::]
 list_df = pd.read_csv(latest_list)
 list_df.columns = ["name","sex","popularity"]
-list_df.to_csv(filename+".csv")
-
+list_df.to_csv("namelist"+year+".csv")
